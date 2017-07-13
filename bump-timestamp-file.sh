b599e5c0421809f135a0d20be5f221399a314f19
dhaneshande <dande@miraclesoft.com>
2017-07-13 13:47:12 -0400

    Add files via upload

---
+++ bump-timestamp-file.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e # fail fast
+set -x # print commands
+
+git clone resource-gist updated-gist
+
+cd updated-gist
+echo $(date) > bumpme
+
+git config --global user.email "dande@miraclesoft.com"
+git config --global user.name "dhaneshande"
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+  
+echo $ PACKAGE_VERSION
+
+git add .
+git commit -m "Bumped date"
